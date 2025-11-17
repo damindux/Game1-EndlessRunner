c0085774e380bf8242d14398a8a56e89c29355ce
Damindu Sandeepa <72655390+damindux@users.noreply.github.com>
2025-11-17 20:28:12 +0530

    Day 04 - Increase Speed overtime
    Info: need to adjust the speed numbers a little bit

--- Assets/Scripts/WorldScroller.cs
+++ Assets/Scripts/WorldScroller.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class WorldScroller : MonoBehaviour {
 	[Header("Scroll Settings")]
 	[SerializeField] private float scrollSpeed = 3f;
+	[SerializeField] private float acceleration = 0.05f;
+	[SerializeField] private float maxSpeed = 15f;
 
 	[Header("Objects to Scroll")]
 	[SerializeField] private string[] scrollTags = { "Ground" };
 
 	private void Update() {
+		// Increase scroll speed over time
+		scrollSpeed += acceleration * Time.deltaTime;
+		if (scrollSpeed > maxSpeed) scrollSpeed = maxSpeed;
+
 		// Move all objects with specified tags to the left
 		foreach (string tag in scrollTags) {
 			GameObject[] objects = GameObject.FindGameObjectsWithTag(tag);
